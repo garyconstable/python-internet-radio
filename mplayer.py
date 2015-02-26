ed4ca9fad631262e53efb3d9d81a57006dc77b14
Gary Constable <garyconstable80@gmail.com>
2015-02-26 00:03:12 +0000

    Add a thread, that will display the current song title.

--- mplayer.py
+++ mplayer.py
@@ -11,49 +11,64 @@
 from Tkinter import *
 import PIL
 from PIL import ImageTk
-from subprocess import call
+import subprocess
+from subprocess import *
+import threading
+from time import sleep
 
 # ---> vars
-maxWidth  	= "320"
-maxHeight 	= "240"
-imgFile   	= "images/skin.jpg"
-volume    	= 0
-volume_mute	= 0
-root		= 0
+maxWidth  	 = "320"
+maxHeight 	 = "240"
+imgFile   	 = "images/skin.jpg"
+volume    	 = 0
+volume_mute	 = 0
+root		 = 0
+songText     = "Most relationships seem so transitory"
+canvas       = ''
+current_song = ''
 
 # ---> functions
 def play():
 	print "----> play"
-	call(["mpc", "play"])
+	subprocess.check_output("mpc play", shell=True)
 
 def pause():
 	print "----> pause"
-	call(["mpc", "pause"])
+	subprocess.check_output("mpc pause", shell=True)
 
 def mute():
 	print "----> mute"
 	global volume
 	global volume_mute
 	if volume == 0:
-		call(["mpc", "volume", str(volume_mute)])
+		subprocess.check_output("mpc volume " + str(volume_mute), shell=True)
 	else:
 		volume_mute = volume
 		volume = 0
-		call(["mpc", "volume", str(volume)])
+		subprocess.check_output("mpc volume " + str(volume), shell=True)
 		
 def volume_up():
 	print "----> volume up"
 	global volume
 	if volume != 100:
 		volume += 10
-		call(["mpc", "volume", str(volume)])
+		subprocess.check_output("mpc volume " + str(volume), shell=True)
 
 def volume_down():	
 	print "----> volume down"
 	global volume
 	if volume != 0:
 		volume -= 10
-		call(["mpc", "volume", str(volume)])
+		subprocess.check_output("mpc volume " + str(volume), shell=True)
+
+def get_current_song():
+	global songText
+	global canvas
+	global current_song
+	songText = subprocess.check_output("mpc current", shell=True)
+	canvas.delete(current_song)
+	current_song = canvas.create_text(30, 20, width=250, anchor="nw", fill="white", font="Purisa",text=songText)
+	
 
 def prev():
 	print "----> prev"
@@ -92,18 +107,40 @@ def callback(event):
 def closeApp():
 	print "----> exit"
 	global root
-	call(["mpc", "stop"])
+	subprocess.check_output("mpc stop", shell=True)
 	root.destroy()
+
+def check_thread():
+	get_current_song()
+	sleep(2.00)
+	check_thread()
+
+def updateGUI():
+    t = threading.Thread(target=check_thread, args=() )
+    t.daemon = True
+    t.start()
+
 
 def init():
 	global volume
 	global volume_mute
 	global root
+	global songText
+	global canvas
+	global current_song
+
 	volume = 50
 	volume_mute = volume
 
-	call(["mpc", "volume", str(volume)])
-	call(["mpc", "stop"])
+	try:
+		subprocess.check_output("mpc volume " + str(volume), shell=True)
+	except (RuntimeError, TypeError, NameError):
+		pass
+
+	try:
+		subprocess.check_output("mpc stop", shell=True)
+	except (RuntimeError, TypeError, NameError):
+		pass
 
 	root = Tk()
 	root.title("MediaPlayer")
@@ -111,17 +148,17 @@ def init():
 	root.geometry(maxWidth+'x'+maxHeight)
 	root.protocol('WM_DELETE_WINDOW', closeApp)
 
-	frame = Frame(root)
-	frame.pack()
-
-	canvas = Canvas(frame, bg="black", width=maxWidth, height=maxHeight)
-	canvas.pack()
-	canvas.bind('<ButtonPress-1>', callback )
-
-	#image  = Image.open(imgFile) 
 	image  = ImageTk.PhotoImage(file=imgFile) 
+
+	canvas = Canvas(root, bg="black", width=maxWidth, height=maxHeight)
 	canvas.create_image(160, 120, image=image)
-	canvas.image = image
+	canvas.bind('<ButtonPress-1>', callback )
+	
+
+	updateGUI()
+
+	
+	canvas.pack()
 	root.mainloop()
 
 
